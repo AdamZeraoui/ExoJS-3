f018676b3696641949bb965241f4df9ed5292fc0
AdamZeraoui <adam.zeroaui@hotmail.fr>
2024-02-28 16:06:26 +0100

    Update

---
+++ rotate.js
@@ -0,0 +1,49 @@
+const squares = document.querySelectorAll(".square")
+
+squares.forEach(function(square){
+    square.addEventListener("click",function(){
+        if()
+    })
+
+    
+});
+
+
+
+
+
+
+
+// const squares=[".squareA",".squareB",".squareC",".squareD"]
+// let countClick = 0
+// // const squareWrapped = document.querySelector("#wrapped")
+
+
+
+// for(let index=0;index<squares.length;index++){
+
+//     if(countClick==0){addEventListener('click',function(){
+//         const square = document.querySelector(squares[index]);
+//     countClick++
+    
+//         square.style.transform = 'rotate(360deg)';
+//         square.style.transition = '2s';
+        
+    
+    
+//     })
+
+//     }else{addEventListener('click',function(){
+//  countClick--
+//         const square = document.querySelector(squares[index]);
+//             square.style.transform = 'rotate(-360deg)';
+//             square.style.transition = '2s';
+        
+        
+//     })
+    
+//     }
+       
+
+
+// }
