81b1b4e537c6d91ab241b866a3acaf1633ec7276
AdamZeraoui <adam.zeraoui@hotmail.fr>
2024-02-29 10:54:53 +0100

    Update

--- rotate.js
+++ rotate.js
@@ -1,49 +1,20 @@
 const squares = document.querySelectorAll(".square")
+const redSquares = document.querySelectorAll(".squareBis")
 
 squares.forEach(function(square){
     square.addEventListener("click",function(){
-        if()
+        if(square.classList == "square"){
+            square.style.transform = 'rotate(360deg)';
+            square.style.transition = '2s';
+            square.classList.toggle("square")
+            square.classList.toggle("squareBis")
+        }else{
+            square.style.transform = 'rotate(0deg)';
+            square.style.transition = '2s';
+            square.classList.toggle("squareBis")
+            square.classList.toggle("square")
+        }
+        
     })
-
-    
+   
 });
-
-
-
-
-
-
-
-// const squares=[".squareA",".squareB",".squareC",".squareD"]
-// let countClick = 0
-// // const squareWrapped = document.querySelector("#wrapped")
-
-
-
-// for(let index=0;index<squares.length;index++){
-
-//     if(countClick==0){addEventListener('click',function(){
-//         const square = document.querySelector(squares[index]);
-//     countClick++
-    
-//         square.style.transform = 'rotate(360deg)';
-//         square.style.transition = '2s';
-        
-    
-    
-//     })
-
-//     }else{addEventListener('click',function(){
-//  countClick--
-//         const square = document.querySelector(squares[index]);
-//             square.style.transform = 'rotate(-360deg)';
-//             square.style.transition = '2s';
-        
-        
-//     })
-    
-//     }
-       
-
-
-// }
